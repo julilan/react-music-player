70f1a16f6ebf89024f48ead3d5052d5888f2f581
julilan <juli.molnar@outlook.com>
2023-08-25 14:07:00 +0300

    wrap inside MusicContext.Provider and add PlayerControls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import { PlayerControls } from './components/PlayerControls';
+import { MusicContext } from './contexts/MusicContext';
 
 function App() {
-  return <div className='App'>App will be here</div>;
+  return (
+    <MusicContext.Provider value={''}>
+      <div className='App'>
+        <PlayerControls />
+      </div>
+    </MusicContext.Provider>
+  );
 }
 
 export default App;
