f09c6b32375521af369ba023e3f08e655982f1ae
julilan <juli.molnar@outlook.com>
2023-08-25 14:13:37 +0300

    add state to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import { PlayerControls } from './components/PlayerControls';
 import { MusicContext } from './contexts/MusicContext';
 
 function App() {
+  const [state, setState] = useState({
+    audioPlayer: new Audio(),
+    tracks: [
+      {
+        name: 'Track 1',
+        file: '',
+      },
+      {
+        name: 'Track 2',
+        file: '',
+      },
+    ],
+    currentTrackIndex: 0,
+    isPlaying: false,
+  });
+
+  console.log('State: ', state);
+
   return (
     <MusicContext.Provider value={''}>
       <div className='App'>
